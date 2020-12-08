94472d7e76454fead3ae65304a1c2dbec0d3ff3b
KANDIKATLA Ravinder <ravinder.kandikatla@hexagon.com>
2020-12-08 08:49:39 +0530

    adding files for github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+import { Component } from "react";
+import { SearchBox } from "./components/search-box/search-box.component";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField:''
+    };
+    //this.handleChange=this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => {
+        this.setState({ monsters: users });
+      });
+  }
+  handleChange=e=>    this.setState({ searchField: e.target.value });
+  
+  render() {
+    const { monsters,searchField } =this.state;
+    const filteredMonsters=monsters.filter(e=>e.name.toLowerCase().includes(searchField.toLowerCase()))
+    return (
+      <div className="App">
+      <h1>Monsters RoleBox</h1>
+     <SearchBox placeholder="Search Monsters" handleChange={this.handleChange}/>
+        <CardList monsters={filteredMonsters}>
+       
+        </CardList>
+        
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './card.styles.css';
+export const Card=props=>{
+    return  <div className="card-container">
+    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set3&size180x80`}/>
+    <h1> {props.monster.name}</h1>
+    <p> {props.monster.email}</p>
+    </div>
+}
